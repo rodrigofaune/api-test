38d456f51f3808f965024c88a33bcb798e31a115
rodrigofaune <rodrigo.faune@gmail.com>
2019-06-18 12:13:10 -0400

    Updates

---
+++ precache-manifest.3100d9dc0d81b66b132f2246e415ba7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824ee6c945a53e7e84bbd9c4f8ff7ff4",
+    "url": "/api-test/index.html"
+  },
+  {
+    "revision": "0737f40f8527ed9629f6",
+    "url": "/api-test/static/css/main.b8cb500e.chunk.css"
+  },
+  {
+    "revision": "65f48283763e18a42cec",
+    "url": "/api-test/static/js/2.389d344c.chunk.js"
+  },
+  {
+    "revision": "0737f40f8527ed9629f6",
+    "url": "/api-test/static/js/main.3c16f138.chunk.js"
+  },
+  {
+    "revision": "e5d5f66c855a3037c324",
+    "url": "/api-test/static/js/runtime~main.3fa43213.js"
+  }
+]);
--- src/redux/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import charactersReducer from './characters'
-import usersReducer from './users'
-
-export default combineReducers({
-  characters: charactersReducer,
-  users: usersReducer,
-})
